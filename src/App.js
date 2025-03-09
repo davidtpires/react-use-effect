2b0b3b03739b0a346ea9074d6e9e0fd41b730ff5
David T. <davidt.info@gmail.com>
2025-03-09 13:14:25 -0300

    loop infinot passando dados para setRacas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [racas, setRacas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/doguinhos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setRacas(dados)
+      })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bem vindo aos doguinhos!</h1>
+      <h4>Confira abaixo uma lista de raças dos doguinhos</h4>
+      <ul>
+        {racas.map(raca => <li key={raca.nome}>{raca.nome}</li>)}
+      </ul>
     </div>
   );
 }
